89f7f34666b6acaf1c0e2a9e2895add5057a0aa8
Jack Xu <jackexyou@gmail.com>
2018-03-30 02:49:18 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -4,4 +4,30 @@ $(document).ready(function(){
 
 // call functions here
 
+function getIt() {
+$('p').on("click", function() {
+  alert("Hey!");
+})
+}
+
+function frameIt() {
+  $('img').on("load", function() {
+    $('img').addClass('tasty')
+  })
+}
+
+function pressIt() {
+  $('input').on('keydown', function(e) {
+    if(e.which === 71) {
+      alert("You pressed 'g!'")
+    }
+  })
+}
+
+function submitIt() {
+  $('form').on('submit', function() {
+    alert("Your form is going to be submitted now.")
+  })
+}
+
 });
